524528e94b70a0595712000c4269be6cf0112323
leoloca98 <leonardolocatelli98@gmail.com>
2021-09-26 22:22:16 +0200

    Create Template

---
+++ index.php
@@ -0,0 +1,29 @@
+<!-- 
+    
+SNACK 1
+Creare una funzione che restituisce un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta. Stampare i numeri in pagina. 
+
+SNACK 2
+Creare un array contenente qualche alunno di un’ipotetica classe.
+Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici (senza, materia, solo numeri). Creare una funzione che calcoli la media voto di un alunno.
+Stampare Nome, Cognome e la media dei voti di ogni alunno.
+
+-->
+
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP SNACKS</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
